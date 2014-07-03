e02803697026830cd9bd748568107358eb7155bb
Matt Felten <matt.felten@dreamhost.com>
2014-07-03 12:12:53 -0700

    Change some JS to make a little more error proof
    Tried adding Reddit RSS feed but didn't work. :/

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,15 @@ jQuery( function($) {
 			url = $feed.data('feed'),
 			count = $feed.data('feed-count');
 
+		if (!count) {
+			count = 6;
+		}
+
 		$.ajax({
 			url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=' + count + '&callback=?&q=' + encodeURIComponent(url),
 			dataType: 'json',
 			success: function(data) {
 				var $ul = $('<ul />'),
-					feed = data.responseData.feed,
 					loadedCount = $body.data('section-loaded')+1,
 					loadedRatio = loadedCount / $body.data('section-count');
 
@@ -26,12 +29,15 @@ jQuery( function($) {
 
 				NProgress.set( loadedRatio );
 
-				$('<a />').attr('href', feed.link ).text( feed.title ).appendTo( $feed ).wrap('<h2 />');
-				$ul.appendTo( $feed );
+				if (data.responseData) {
+					var feed = data.responseData.feed;
+					$('<a />').attr('href', feed.link ).text( feed.title ).appendTo( $feed ).wrap('<h2 />');
+					$ul.appendTo( $feed );
 
-				feed.entries.forEach( function(post) {
-					$('<a />').text( post.title ).attr('href', post.link ).appendTo( $ul ).wrap('<li />');
-				});
+					feed.entries.forEach( function(post) {
+						$('<a />').text( post.title ).attr('href', post.link ).appendTo( $ul ).wrap('<li />');
+					});
+				}
 			}
 		});
 	});
